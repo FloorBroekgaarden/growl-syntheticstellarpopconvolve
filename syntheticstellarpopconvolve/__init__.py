b99527220b3731b9e4a7d4b2c6dcab8c2a1e7f1d
David Hendriks <davidhendriks93@gmail.com>
2024-04-29 00:07:39 +0100

    cleaning up code imports

---
+++ syntheticstellarpopconvolve/__init__.py
@@ -0,0 +1,10 @@
+import copy
+
+import syntheticstellarpopconvolve as sspc
+
+from sspc.convolution_default_settings import (
+    convolution_options_defaults,
+)
+from sspc.convolve import convolve  # noqa: F401
+
+convolution_config = copy.copy(convolution_options_defaults)

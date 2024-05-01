c658e59d56d1a59bee1d86437582f8e6813ab101
David Hendriks <davidhendriks93@gmail.com>
2024-05-01 04:16:03 +0100

    removed pycache, updated requirements, fixed init

--- syntheticstellarpopconvolve/__init__.py
+++ syntheticstellarpopconvolve/__init__.py
@@ -1,6 +1,8 @@
 import copy
 
-from sspc.convolution_default_settings import convolution_options_defaults
-from sspc.convolve import convolve  # noqa: F401
+from syntheticstellarpopconvolve.convolution_default_settings import (
+    convolution_options_defaults,
+)
+from syntheticstellarpopconvolve.convolve import convolve  # noqa: F401
 
 convolution_config = copy.copy(convolution_options_defaults)

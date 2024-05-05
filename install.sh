7536b5025d3258d1fa88228683e6514e50c02d01
David Hendriks <davidhendriks93@gmail.com>
2024-05-06 00:09:46 +0100

    working on the documentation

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script to install syntheticstellarpopconvolve in the current venv
+
+VERSION_NUMBER=$(cat "VERSION")
+echo "installing syntheticstellarpopconvolve version $VERSION_NUMBER"
+
+# we can only use python3 and python3, but allow
+# the user to set these in environment variables
+# PYTHON and PIP.
+: "${PYTHON:="python3"}"
+: "${PIP:="pip3"}"
+
+# do stuff...
+$PYTHON setup.py clean
+cd docs
+$PIP uninstall -y syntheticstellarpopconvolve
+cd ../
+$PYTHON setup.py build --force
+$PYTHON setup.py sdist
+$PIP install -v dist/syntheticstellarpopconvolve-$VERSION_NUMBER.tar.gz

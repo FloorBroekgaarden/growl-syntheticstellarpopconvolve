70cd82b3ac579c707bab86edbc86c0e46586c303
David Hendriks <davidhendriks93@gmail.com>
2024-05-24 18:22:44 +0100

    putting structure in for sampling of systems

--- syntheticstellarpopconvolve/convolve_stochastically.py
+++ syntheticstellarpopconvolve/convolve_stochastically.py
@@ -1,5 +1,4 @@
-"""
-Routines for stochastic convolution
+"""Routines for stochastic convolution
 
 
 initial idea with simple situation
@@ -34,8 +33,11 @@ assign radnom position
 - can also easily be extended to include metallicity
 - naturally handles unequal yield per systems
 
-- this method does not turn things around like the others do. We start at a given lookback time bin for all systems, and we let the systems be born
 
+
+Notes:
+- this method does not turn things around like the others do. We start
+at a given lookback time bin for all systems. We sample a set of systems based on the total starformation within that lookback time bin, and the normalized yields of the systems. We then assign a birth lookback time to the systems (taken randomly between the bin edges)
 """
 
 import astropy.units as u
@@ -45,7 +47,6 @@ import numpy as np
 lookback_time_index = 5
 scale_factor = 1e-8
 size = 10
-
 
 # have some starformation array
 lookback_time_bin_edges = (np.arange(0, 10, 1) * u.Gyr).to(u.yr)
@@ -65,46 +66,113 @@ print(total_star_formation_at_lookback_times)
 normalized_yield_array = scale_factor * np.random.random(size=size) * (1 / u.Msun)
 print("normalized_yield_array", normalized_yield_array)
 
-# calculate the formation yield of all the systems
-formation_yield = (
-    total_star_formation_at_lookback_times[lookback_time_index] * normalized_yield_array
+data_dict = {}
+data_dict["normalized_yield_array"] = normalized_yield_array
+data_dict["IDs"] = np.arange(len(normalized_yield_array))
+
+
+def sample_systems(
+    total_star_formation_in_bin,
+    lookback_time_bin_size,
+    lookback_time_bin_lower_edge,
+    data_dict,
+):
+    """
+    General function to handle sampling a set of systems based on
+    normalized yields and a total mass of stars formed
+
+    # TODO: we work with indices now, which might not work when we slice and dice.
+    # TODO: or we should make sure the indices map back to IDs
+    """
+
+    # calculate the formation yield of all the systems
+    formation_yield = total_star_formation_in_bin * data_dict["normalized_yield_array"]
+    print("formation_yield", formation_yield)
+
+    # select those that have > 1:
+    integer_formations = np.array(np.floor(formation_yield), dtype=int)
+    print("integer_formations", integer_formations)
+
+    # select the remainder
+    fractional_formations = formation_yield - integer_formations
+    print("fractional_formations", fractional_formations)
+
+    # take a random set to sample the fractional formations
+    random_chance = np.random.random(fractional_formations.shape)
+    print("random_chance", random_chance)
+
+    fractional_formations_sampled = random_chance < fractional_formations
+    print("fractional_formations_sampled", fractional_formations_sampled)
+
+    #
+    integer_formation_IDs = np.repeat(data_dict["IDs"], integer_formations)
+    print("integer_formation_indices", integer_formation_IDs)
+
+    fractional_formation_IDs = data_dict["IDs"][fractional_formations_sampled]
+    print("fractional_formation_indices", fractional_formation_IDs)
+
+    combined_IDs = np.concatenate([integer_formation_IDs, fractional_formation_IDs])
+    print("combined_indices", combined_IDs)
+
+    # Assign random formation times (of system)
+    sampled_formation_lookback_times = (
+        np.random.random(size=size) * lookback_time_bin_size
+    ) + lookback_time_bin_lower_edge
+    print("sampled_formation_lookback_times", sampled_formation_lookback_times)
+
+    return combined_IDs, sampled_formation_lookback_times
+
+
+def sample_systems_main(
+    total_star_formation_in_lookback_time_bin,
+    lookback_time_bin_size,
+    lookback_time_bin_lower_edge,
+    data_dict,
+    metallicity_distribution_at_lookback_time=None,
+    metallicity_bins=None,
+):
+    """
+    Function that handles sampling systems.
+
+    Currently only supports sampling without metallicity dependence
+
+    TODO: add support for metallicity specific sampling
+    NOTE: data_dict should contain IDs of some sort
+    """
+
+    ############
+    # Method 1: no metallicity dependence
+    if metallicity_distribution_at_lookback_time is None:
+
+        combined_IDs, sampled_formation_lookback_times = sample_systems(
+            total_star_formation_in_bin=total_star_formation_in_lookback_time_bin,
+            data_dict=data_dict,
+            lookback_time_bin_size=lookback_time_bin_size,
+            lookback_time_bin_lower_edge=lookback_time_bin_lower_edge,
+        )
+
+        print(combined_IDs, sampled_formation_lookback_times)
+    ############
+    # Method 2: metallicity dependence
+    else:
+
+        # TODO: loop over the metallicity bins
+        # TODO: calculate the total mass formed in that metallicity bin
+        # TODO: query the data dict for all systems in the current metallicity bin
+        # TODO: create a data dict for this particular metallicity
+        # TODO: store/append to combined array
+        # TODO: we work with indices now, which might not work when we slice and dice.
+        # TODO: or we should make sure the indices map back to IDs
+        raise ValueError("Sampling with metallicity distribution is not supported yet")
+
+
+sample_systems_main(
+    total_star_formation_in_lookback_time_bin=total_star_formation_at_lookback_times[
+        lookback_time_index
+    ],
+    data_dict=data_dict,
+    lookback_time_bin_lower_edge=lookback_time_bin_edges[lookback_time_index],
+    lookback_time_bin_size=bin_sizes[lookback_time_index],
+    metallicity_distribution_at_lookback_time=None,
+    metallicity_bins=None,
 )
-print("formation_yield", formation_yield)
-
-# select those that have > 1:
-integer_formations = np.array(np.floor(formation_yield), dtype=int)
-print("integer_formations", integer_formations)
-
-# select the remainder
-fractional_formations = formation_yield - integer_formations
-print("fractional_formations", fractional_formations)
-
-# take a random set to sample the fractional formations
-random_chance = np.random.random(fractional_formations.shape)
-print("random_chance", random_chance)
-
-fractional_formations_sampled = random_chance < fractional_formations
-print("fractional_formations_sampled", fractional_formations_sampled)
-
-# Get the indices
-index_array = np.arange(len(formation_yield))
-
-integer_formation_indices = np.repeat(index_array, integer_formations)
-print("integer_formation_indices", integer_formation_indices)
-
-fractional_formation_indices = index_array[fractional_formations_sampled]
-print("fractional_formation_indices", fractional_formation_indices)
-
-combined_indices = np.concatenate(
-    [integer_formation_indices, fractional_formation_indices]
-)
-print("combined_indices", combined_indices)
-
-# Assign random formation times (of system)
-sampled_formation_lookback_times = (
-    np.random.random(size=size) * bin_sizes[lookback_time_index]
-) + lookback_time_bin_edges[lookback_time_index]
-print("sampled_formation_lookback_times", sampled_formation_lookback_times)
-
-# TODO: also assign in
-# TODO: glue together and store

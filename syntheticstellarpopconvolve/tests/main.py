003532b9dc26b1cc6250e07e6667b02e8f42f0b8
david hendriks <davidhendriks93@gmail.com>
2025-01-22 23:47:14 +0000

    fixed tests for ensemble utils. updated reports

--- syntheticstellarpopconvolve/tests/main.py
+++ syntheticstellarpopconvolve/tests/main.py
@@ -43,32 +43,11 @@ from syntheticstellarpopconvolve.tests.test_convolve_custom_data import (
     test_extract_custom_data,
 )
 from syntheticstellarpopconvolve.tests.test_convolve_ensembles import (
-    test__get_ensemble_structure,
-    test_attach_endpoints,
-    test_check_if_value_layer,
-    test_check_if_value_layer_and_get_layer_iterable,
     test_convolve_ensemble_by_integration,
     test_ensemble_handle_marginalisation,
     test_ensemble_handle_SFR_multiplication,
-    test_ensemble_marginalise_layer,
-    test_extract_endpoints,
     test_extract_ensemble_data,
-    test_get_data_layer_dict_values,
-    test_get_deepest_data_layer_depth,
-    test_get_depth_ensemble_all_endpoints,
-    test_get_depth_ensemble_first_endpoint,
-    test_get_ensemble_binsizes,
-    test_get_ensemble_structure,
-    test_get_layer_iterable,
-    test_get_max_depth_ensemble,
     test_handle_binsize_multiplication_factor,
-    test_invert_data_layer_dict,
-    test_multiply_ensemble,
-    test_set_endpoints,
-    test_shift_data_layer,
-    test_shift_layers_dict,
-    test_shift_layers_list,
-    test_strip_ensemble_endpoints,
 )
 from syntheticstellarpopconvolve.tests.test_convolve_events import (
     test_convolve_events_by_integration,
@@ -91,6 +70,7 @@ from syntheticstellarpopconvolve.tests.test_default_convolution_config import (
     test_logger_validation,
     test_unit_validation,
 )
+from syntheticstellarpopconvolve.tests.test_ensemble_utils import *
 from syntheticstellarpopconvolve.tests.test_extract_population_settings import (
     test_extract_population_settings,
 )

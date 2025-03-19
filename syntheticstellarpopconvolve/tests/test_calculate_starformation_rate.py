6065e8681e1190ab09fd530d12127155358eb278
david hendriks <davidhendriks93@gmail.com>
2025-03-19 18:39:38 +0000

    fixed failing tests

--- syntheticstellarpopconvolve/tests/test_calculate_starformation_rate.py
+++ syntheticstellarpopconvolve/tests/test_calculate_starformation_rate.py
@@ -42,6 +42,146 @@ TMP_DIR = temp_dir(
     clean_path=True,
 )
 
+# class test_calculate_digitized_sfr_rates(unittest.TestCase):
+#     def setUp(self):
+#         #
+#         input_hdf5_filename = os.path.join(TMP_DIR, "input_hdf5_sfr_only.h5")
+#         output_hdf5_filename = os.path.join(TMP_DIR, "output_hdf5_sfr_only.h5")
+
+#         ##############
+#         # SET UP DATA
+#         self.dummy_data = {
+#             "delay_time": np.array([0, 1, 2, 3]) * u.yr,
+#             "probability": np.array([1, 2, 3, 4]),
+#         }
+#         dummy_df = pd.DataFrame.from_records(self.dummy_data)
+
+#         #############
+#         # create input HDF5 file
+#         with h5py.File(input_hdf5_filename, "w") as input_hdf5_file:
+
+#             ######################
+#             # Create groups
+#             input_hdf5_file.create_group("input_data")
+#             input_hdf5_file.create_group("config")
+
+#             ###############
+#             # Readout population settings
+#             population_settings_filename = pkg_resources.resource_filename(
+#                 "syntheticstellarpopconvolve",
+#                 "example_data/example_population_settings.json",
+#             )
+
+#             with open(population_settings_filename, "r") as f:
+#                 population_settings = json.loads(f.read())
+
+#             # Delete some stuff from the settings
+#             del population_settings["population_settings"]["bse_options"]["metallicity"]
+
+#             # Write population config to file
+#             input_hdf5_file.create_dataset(
+#                 "config/population", data=json.dumps(population_settings)
+#             )
+
+#         ##############
+#         # Store data in pandas
+#         dummy_df.to_hdf(input_hdf5_filename, key="input_data/{}".format("dummy"))
+
+#         #
+#         self.convolution_config = copy.copy(default_convolution_config)
+
+#         # Set up SFR
+#         self.convolution_config["SFR_info"] = {
+#             "lookback_time_bin_edges": np.array([0, 1, 2, 3, 4, 5]) * 1e9 * u.yr,
+#             "starformation_rate_array": np.array([1, 2, 3, 4, 5])
+#             * u.Msun
+#             / u.yr
+#             / u.Gpc**3,
+#         }
+
+#         # set up convolution bins
+#         self.convolution_config["convolution_lookback_time_bin_edges"] = (
+#             np.array([0, 1, 2, 3, 4]) * 1e9 * u.yr
+#         )
+
+#         # lookback time convolution only
+#         self.convolution_config["time_type"] = "lookback_time"
+
+#         #
+#         self.convolution_config["input_filename"] = input_hdf5_filename
+#         self.convolution_config["output_filename"] = output_hdf5_filename
+
+#         self.convolution_config["redshift_interpolator_data_output_filename"] = (
+#             os.path.join(TMP_DIR, "interpolator_dict.p")
+#         )
+
+#         #
+#         self.convolution_config["convolution_instructions"] = [
+#             {
+#                 **default_convolution_instruction,
+#                 "input_data_name": "dummy",
+#                 "output_data_name": "dummy",
+#                 "convolution_type": "integrate",
+#                 "data_column_dict": {
+#                     "delay_time": "delay_time",
+#                     "normalized_yield": "probability",
+#                 },
+#             },
+#         ]
+
+#         #
+#         self.convolution_config["tmp_dir"] = os.path.join(TMP_DIR, "tmp")
+
+#         #
+#         check_and_update_convolution_config(self.convolution_config)
+
+#         #
+#         check_and_prepare_output_file(config=self.convolution_config)
+
+#     def test_calculate_digitized_sfr_rates_sfr_only(self):
+
+#         digitized_sfr_rates = calculate_digitized_sfr_rates(
+#             config=self.convolution_config,
+#             convolution_time_bin_center=0.5 * 1e9 * u.yr,
+#             data_dict={"delay_time": np.array([-1, 1, 2, 3, 100]) * 1e9 * u.yr},
+#             sfr_dict=self.convolution_config["SFR_info"],
+#             convolution_instruction=self.convolution_config["convolution_instructions"][
+#                 0
+#             ],
+#         )
+#         output_unit = u.Msun / u.yr / u.Gpc**3
+
+#         np.testing.assert_array_equal(
+#             digitized_sfr_rates, np.array([0.0, 2.0, 3.0, 4.0, 0.0]) * output_unit
+#         )
+
+#     # def test_calculate_digitized_sfr_rates_metallicity(self):
+
+#     #     self.convolution_config["SFR_info"]["metallicity_bin_edges"] = (
+#     #         self.convolution_config["SFR_info"]["starformation_array"]
+#     #         * np.ones(
+#     #             (self.convolution_config["SFR_info"]["starformation_array"].shape[0], 3)
+#     #         ).T
+#     #     )
+
+#     #     # print(self.convolution_config["SFR_info"]["metallicity_bin_edges"])
+
+#     #     #
+#     #     sfr_dict = update_sfr_dict(
+#     #         sfr_dict=self.convolution_config["SFR_info"], config=self.convolution_config
+#     #     )
+
+#     #     digitized_sfr_rates = calculate_digitized_sfr_rates(
+#     #         config=self.convolution_config,
+#     #         convolution_time_bin_center=0.5 * 1e9 * u.yr,
+#     #         data_dict={"delay_time": np.array([-1, 1, 2, 3, 100]) * 1e9},
+#     #         sfr_dict=sfr_dict,
+#     #     )
+
+#     #     np.testing.assert_array_equal(
+#     #         digitized_sfr_rates, np.array([0.0, 2.0, 3.0, 4.0, 0.0])
+#     #     )
+
 
 class test_general_sfr_digitise_function(unittest.TestCase):
     def test_general_sfr_digitise_function_absolute(self):
